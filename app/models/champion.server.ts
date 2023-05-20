adc220aba02f967bf5373daa57a718398fbae5c7
Quentin Gibson <quentingibson94@gmail.com>
2023-05-20 13:04:37 -0400

    empty crud functions

--- app/models/champion.server.ts
+++ app/models/champion.server.ts
@@ -1,3 +1,11 @@
 import {prisma} from '~/db.server'
-export function createChampion(championData: any) {
-}
+
+export function createChampion(championData: any) {}
+
+export function updateChampion(championID: string, championData: any) {}
+
+export function getChampionByID(championID: string) { }
+
+export function getChampions() { }
+
+export function deleteChampionByID(championID: string) { }
---
+++ app/models/type.server.ts
@@ -0,0 +1,12 @@
+import { prisma } from '~/db.server'
+
+export function createType(typeData: any) {}
+
+
+export function updateType(typeID: string, typeData: any) {}
+
+export function getTypeByID(typeID: string) { }
+
+export function getTypes() { }
+
+export function deleteTypeByID(typeID: string) { }
