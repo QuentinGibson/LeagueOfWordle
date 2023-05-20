1dbad6906cb31606092e5d8037283a1fdfc9a531
Quentin Gibson <quentingibson94@gmail.com>
2023-05-20 11:57:12 -0400

    set title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
-export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
+export const meta: V2_MetaFunction = () => [{ title: "League Of Wordle" }];
 
 export default function Index() {
   const user = useOptionalUser();
