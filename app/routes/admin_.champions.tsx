acde745071d8fcff88736f2d4dd19fe647d103c4
Quentin Gibson <quentingibson94@gmail.com>
2023-05-21 03:11:02 -0400

    fix update champion route

--- app/routes/admin_.champions.tsx
+++ app/routes/admin_.champions.tsx
@@ -9,13 +9,17 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 export default function ComponentName() {
   const { champions } = useLoaderData<typeof loader>()
   return (
-    <main>
-      <h1>Champions</h1>
-      <ul>
+    <main className="py-8 px-4">
+      <h1 className="font-bold text-5xl mb-8">Champions</h1>
+      <ul className="grid gap-4">
         {champions.map(champion => (
-          <li className="flex">
-            {champion.name}
-            <Link to={`/admin/update_${champion.id}`}>Update Champion</Link>
+          <li className="flex grid-cols-3 gap-2 items-center">
+            <div className="flex basis-36">
+              <p className="font-bold">{champion.name}</p>
+            </div>
+            <div className="flex">
+              <Link className="px-4 py-2 bg-indigo-700" to={`/admin/champion/${champion.id}`}>Update Champion</Link>
+            </div>
             <Form method="POST" action="/api/delete">
               <button type="submit" className="px-4 py-2 bg-red-500">Delete Champion</button>
               <input type="hidden" name="championID" value={champion.id} />
